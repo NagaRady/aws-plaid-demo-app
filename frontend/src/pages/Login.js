111e66572ec39b62339e73f7894064af84f20757
mtarakavignesh3 <tvm3@illinois.edu>
2024-09-17 00:06:21 -0500

    Enhanced Password Policy in Signup Page

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -56,7 +56,7 @@ export default function Login() {
               <strong>Password Policy</strong>
               <span className="question-mark" onClick={togglePolicy}>
                 {" "}
-                (What's this?)
+                (?)
               </span>
 
               {/* Password requirements - conditionally shown */}
