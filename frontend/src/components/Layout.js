d363f7ebb362ab24ea705128eb75cb630fe87c99
Naga Mullapudi <harishmullapudi@gmail.com>
2024-09-16 20:52:27 -0700

    removing name duplication on login screen

--- frontend/src/components/Layout.js
+++ frontend/src/components/Layout.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Outlet, useNavigate } from 'react-router-dom';
-import { useAuthenticator, Heading, View, Text } from '@aws-amplify/ui-react';
+import { useAuthenticator, View, Text } from '@aws-amplify/ui-react';
 
 export default function Layout() {
   const { route, signOut, user } = useAuthenticator((context) => [
@@ -17,13 +17,8 @@ export default function Layout() {
 
   return (
     <>
-      {/* Conditionally display the heading only when the user is NOT authenticated */}
-      {route !== 'authenticated' && (
-        <Heading level={2} className="custom-heading">
-          The journey to safe payments begins here
-        </Heading>
-      )}
-
+      {/* Remove the heading that displays "The journey to safe payments begins here" */}
+      
       {/* Display the login/signup message or welcome message based on authentication */}
       <View>
         <Text className="custom-login-text">
